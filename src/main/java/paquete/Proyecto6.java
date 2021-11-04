3e16c1926bdde38aef10441dc5e17a83ee32b648
NsoEnigma <alexcelia910@gmail.com>
2021-11-04 13:01:46 +0100

    proyecto completo

---
+++ src/main/java/paquete/Proyecto6.java
@@ -0,0 +1,156 @@
+package paquete;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoField;
+import java.util.Scanner;
+
+public class Proyecto6 {
+	
+	public static void main(String[] args) {
+		Scanner lector = new Scanner(System.in);
+	
+		System.out.println("Dime el nombre de la primera persona");
+		String nombre1 = lector.nextLine();
+		System.out.println("Dime su año de nacimiento");
+		short anioNacimiento1 = Short.parseShort(lector.nextLine());
+		System.out.println("Ahora dime su genero (h/m)");
+		char genero1 = lector.nextLine().charAt(0);
+		System.out.println("Ahora dime su altura en metros");
+		float altura1 = Float.parseFloat(lector.nextLine());
+		
+		System.out.println("Dime el nombre de la segunda persona");
+		String nombre2 = lector.nextLine();
+		System.out.println("Dime su año de nacimiento");
+		short anioNacimiento2 = Short.parseShort(lector.nextLine());
+		System.out.println("Ahora dime su genero (h/m)");
+		char genero2 = lector.nextLine().charAt(0);
+		System.out.println("Ahora dime su altura en metros");
+		float altura2 = Float.parseFloat(lector.nextLine());
+		
+		System.out.println("Dime el nombre de la tercera persona");
+		String nombre3 = lector.nextLine();
+		System.out.println("Dime su año de nacimiento");
+		short anioNacimiento3 = Short.parseShort(lector.nextLine());
+		System.out.println("Ahora dime su genero (h/m)");
+		char genero3 = lector.nextLine().charAt(0);
+		System.out.println("Ahora dime su altura en metros");
+		float altura3 = Float.parseFloat(lector.nextLine());
+		
+		float centimetro1 = altura1*100;
+		float centimetro2 = altura2*100;
+		float centimetro3 = altura3*100;
+		
+		double media = (centimetro1 + centimetro2 + centimetro3)/3;
+		
+		System.out.println("La media en centimetros de las 3 alturas es: " + media + "cm");
+		
+		System.out.println("Suponiendo que estamos en el año 2021 las personas han nacido en el siglo: ");
+		
+		int edad1 = 2021-anioNacimiento1;
+		int edad2 = 2021-anioNacimiento2;
+		int edad3 = 2021-anioNacimiento3;
+		
+		
+		if (anioNacimiento1 < 2000) {
+			System.out.println("La primera persona nacio en el siglo XX");
+		}
+		
+		if (anioNacimiento2 < 2000) {
+			System.out.println("La segunda persona nacio en el siglo XX");
+		}
+		
+		if (anioNacimiento3 < 2000) {
+			System.out.println("La tercera persona nacio en el siglo XX");
+		}
+		
+		if (anioNacimiento1 >= 2000) {
+			System.out.println("La primera persona nacio en el siglo XXI");
+		}
+		
+		if (anioNacimiento2 >= 2000) {
+			System.out.println("La segunda persona nacio en el siglo XXI");
+		}
+		
+		if (anioNacimiento3 >= 2000) {
+			System.out.println("La tercera persona nacio en el siglo XXI");
+		}
+		
+		
+		System.out.println("La edad de la primera persona es " + edad1 + " años");
+		System.out.println("La edad de la segunda persona es " + edad2 + " años");
+		System.out.println("La edad de la tercera persona es " + edad3 + " años");
+		
+		byte contHombre = 0;
+		byte contMujer = 0;
+		
+		if (genero1 == 'h') {
+			contHombre += 1;
+		}
+		
+		if (genero2 == 'h') {
+			contHombre += 1;
+		}
+		
+		if (genero3 == 'h') {
+			contHombre += 1;
+		}
+		
+		if (genero1 == 'm') {
+			contMujer += 1;
+		}
+		
+		if (genero2 == 'm') {
+			contMujer += 1;
+		}
+		
+		if (genero3 == 'm') {
+			contMujer += 1;
+		}
+		
+		System.out.println("Hay un total de " + contHombre + " hombres.");
+		System.out.println("Hay un total de " + contMujer + " mujeres.");
+		
+		if (nombre1.length() > nombre2.length() && nombre1.length() > nombre3.length()) {
+			System.out.println("El nombre mas largo de los 3 es " + nombre1);
+		}
+		
+		if (nombre2.length() > nombre1.length() && nombre2.length() > nombre1.length()) {
+			System.out.println("El nombre mas largo de los 3 es " + nombre2);
+		}
+		
+		if (nombre3.length() > nombre1.length() && nombre3.length() > nombre2.length()) {
+			System.out.println("El nombre mas largo de los 3 es " + nombre3);
+		}
+		
+		int dias1 = ((2021 - anioNacimiento1) * 365);
+		int dias2 = ((2021 - anioNacimiento2) * 365);
+		int dias3 = ((2021 - anioNacimiento3) * 365);
+		
+		
+		System.out.println("Suponiendo que estamos en el año 2021 la primera persona ha vivido unos " + dias1 + " días.");
+		System.out.println("Suponiendo que estamos en el año 2021 la segunda persona ha vivido unos " + dias2 + " días.");
+		System.out.println("Suponiendo que estamos en el año 2021 la tercera persona ha vivido unos " + dias3 + " días.");
+		
+		System.out.println("Suponiendo que estamos en el año 2021 la primera persona ha vivido unos " + ((2021 - anioNacimiento1) * 365)*24 + " horas." );
+		System.out.println("Suponiendo que estamos en el año 2021 la segunda persona ha vivido unos " + ((2021 - anioNacimiento2) * 365)*24 + " horas." );
+		System.out.println("Suponiendo que estamos en el año 2021 la tercera persona ha vivido unos " + ((2021 - anioNacimiento3) * 365)*24 + " horas." );
+		
+		int promedio = ((dias1 + dias2 + dias3) /3);
+		
+		System.out.println("Las tres personas juntas han vividos un total de " + promedio + " dias.") ;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
